a032b7516a69fda295ea886e3ce9df331c4a0b9c
maurice <sabertoothmaurice@gmail.com>
2015-01-02 02:03:10 +0530

    added support till online game connecting playerss

--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,7 @@ var fn = require('../lib/common-utils/functions');
 
 var userIdGenerator = 0;
 var users = {};
+var freeUser = null;
 function userObject(socket){
     this.socket = socket;
     this.userId = userIdGenerator++;
@@ -29,18 +30,17 @@ var GameServer = function (app, io) {
 
     });
     io.on('connection', function(socket){
-        console.log('a new user connected');
         var user = new userObject(socket);
+        console.log('a new user connected:'+user.userId);
         users[user.userId]=user;
-//        this.mainId = user.userId;
         socket.emit('new connection',{ id : user.userId});
         socket.userId = user.userId;
-        this.opponent = null;
+        socket.opponent = null;
 
 
         //SERVER RELATED CALLS
         socket.on('all users', function(){
-            console.log('User requested all users details');
+            console.log('User: all users');
             io.emit('all users', getUsersOnline());
         });
 
@@ -57,8 +57,13 @@ var GameServer = function (app, io) {
 //            for(var i =0;i<users.length;i++){
 //                usernames.push(users[i].userId);
 //            }
+
+            if(users[socket.userId].opponent!=null)
+                if (users[users[socket.userId].opponent]!=null)
+                    users[users[socket.userId].opponent].socket.emit("opponent disconnected",{id:socket.userId});
             delete users[socket.userId];
-            io.emit('all users', null);
+            if(freeUser == socket.userId) freeUser=null;
+            io.emit('all users', getUsersOnline());
         });
 
         //USER CONNECTING TO ANOTHER USER
@@ -98,11 +103,54 @@ var GameServer = function (app, io) {
 
         //CONNECTED USER COMMANDS
         socket.on('command', function(msg){
+            console.log("User: command:"+JSON.stringify(msg));
             if(users[msg.userId1].opponent == null) return;
             if(users[msg.userId2].opponent == null) return;
-            console.log("CMD:"+JSON.stringify(msg));
             users[msg.userId1].socket.emit('command',msg);
             users[msg.userId2].socket.emit('command',msg);
+        });
+
+        //NEW PROTOCOL
+        socket.on('request free user', function(msg){
+            console.log('User: request free user'+JSON.stringify(msg));
+            var reply = {};
+            if(freeUser==null){
+                freeUser=msg.id;
+                reply.success = false;
+                socket.emit('waiting for another free user', reply);
+                console.log('sent: waiting for another free user:'+JSON.stringify(reply));
+            }else{
+                reply.userId1 = msg.id;
+                console.log('LOGGER: freeUser:'+freeUser);
+                console.log('LOGGER: users:'+printUsers());
+                users[freeUser].socket.emit('u wanna be opponent',reply);
+                console.log('sent: u wanna be opponent:'+JSON.stringify(reply));
+            }
+        });
+        socket.on('u wanna be opponent', function(msg){
+            console.log('User: u wanna be opponent:'+JSON.stringify(msg));
+            if(msg.accept){
+                users[msg.userId2].opponent  =users[msg.userId1].userId;
+                users[msg.userId1].opponent  =users[msg.userId2].userId;
+                users[msg.userId2].socket.emit("opponent connected",{userId1:msg.userId1,userId2:msg.userId2});
+                console.log('sent: opponent connected:');
+                users[msg.userId1].socket.emit("opponent connected",{userId1:msg.userId1,userId2:msg.userId2});
+                console.log('sent: opponent connected:');
+            }else{
+                //rare
+            }
+        });
+        socket.on('reply opponent request', function(msg){
+            console.log('User: reply opponent request:'+JSON.stringify(msg));
+            var reply = {};
+            if(freeUser==null){
+                freeUser=msg.id;
+                reply.success = false;
+                socket.emit('waiting for another free user', reply);
+                console.log('sent: waiting for another free user:'+JSON.stringify(reply));
+            }else{
+                users[freeUser].socket.emit('u wanna be opponent',reply);
+            }
         });
 
     });
